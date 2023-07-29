72bfab4eef2c3ca6e249ed0caee9f3e69346858b
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-29 14:41:35 +0200

    add-name==main-test1-test2-test1

---
+++ test1.py
@@ -0,0 +1,14 @@
+print('i am from test1')
+
+def add(x,y):
+    return x+y
+
+
+def mul(x,y):
+    return x*y
+#if __name__=='__main__':
+    print(add(6,2))
+    print(mul(6,2))
+
+print(add(6,2))
+print(mul(6,2))
---
+++ test3.py
@@ -0,0 +1,5 @@
+import test1
+
+
+print(test1.add(6,2))
+print(test1.mul(6,2))
