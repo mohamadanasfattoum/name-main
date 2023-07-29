1940a03782cf2552c6e41cee1fdf88c8bd512c0e
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-29 14:51:00 +0200

    add-name==main-test1-test2-test1-end with explain

--- test1.py
+++ test1.py
@@ -6,9 +6,11 @@ def add(x,y):
 
 def mul(x,y):
     return x*y
-#if __name__=='__main__':
-    print(add(6,2))
-    print(mul(6,2))
+if __name__=='__main__':
+    print('you can not run from test1')
 
-print(add(6,2))
-print(mul(6,2))
+
+#print(add(6,2))
+#print(mul(6,2))
+# in den haupt seit ist name = main, deshalb kann man hier starten.
+# aber was auserhalb name=='main' kann man von jeder seite run machen oder den function rufen
